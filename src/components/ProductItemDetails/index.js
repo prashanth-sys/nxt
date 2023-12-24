978ed38a55b8f4d730dcf1d4f0b73fb5e78c283a
Your Name <reddyprashanth512@gmail.com>
2023-12-24 08:28:38 +0000

    solution

---
+++ src/components/ProductItemDetails/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import Header from '../Header'
+import './index.css'
+
+const ProductItemDetails = () => (
+  <>
+    <Header />
+  </>
+)
+export default ProductItemDetails
